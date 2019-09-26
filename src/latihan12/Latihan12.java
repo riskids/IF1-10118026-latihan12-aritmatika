4df4072f2137649cfad4c883269ca23c0e4bca96
Riski Dwi <bintangmalamhari@gmail.com>
2019-09-26 12:47:42 +0700

    Add files via upload

---
+++ src/latihan12/Latihan12.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan12;
+
+/**
+ *
+ * @author dwisr
+ * NAMA      : RISKI DWI SABARIYANTO
+ * KELAS     : IF -1 
+ * NIM       : 10118026
+ * Deskripsi : Program untuk belajar aritmatika dalam koding
+ * 
+ */
+public class Latihan12 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a = 10;
+        int b = 20;
+        int c = 25;
+        int d = 25;
+        System.out.println("a + b = " + (a+b) );
+        System.out.println("a - b = " + (a - b) );
+        System.out.println("a * b = " + (a * b) );
+        System.out.println("b / a = " + (b / a) );
+        System.out.println("b % a = " + (b % a) );
+        System.out.println("c % a = " + (c % a) );
+        System.out.println("a++ = " + (a++) );
+        System.out.println("b-- = " + (b--) );
+        System.out.println("d++ = " + (d++) );
+        System.out.println("++d = " + (++d) );
+    }
+    
+}
